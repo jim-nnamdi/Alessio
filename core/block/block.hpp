3382fae8d73023b323b51ea66254879026aa4028
Jim <jimsamuel50@gmail.com>
2024-06-05 11:58:13 +0100

    Alogic implementation and remove previous driver code for blocks

--- core/block/block.hpp
+++ core/block/block.hpp
@@ -7,11 +7,12 @@
 #include <ctime>
 #include <utility>
 #include <string>
+#include <type_traits>
 
 struct extra{
     time_t stamp;
-    const char* rev;    
-    const char* aev;
+    ssize_t rev;    
+    ssize_t aev;
     std::string tstamp;
 };
 
@@ -32,3 +33,28 @@ template<typename A>
 void end_alessio(struct alessio<A>* agen, std::vector<A> aev);
 template<typename A>
 struct alessio<A> arbitrary_pos(struct alessio<A>* agen, std::vector<A> aev, int pos);
+
+template<typename T>
+class Alogic{
+    public:
+    Alogic(){
+        std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+        std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
+        std::string now_str = std::ctime(&now_time_t);
+        struct extra aextra;
+        ssize_t real_compute = aextra.stamp ^ aextra.stamp << 1 ^ aextra.stamp << 2;
+        aextra.aev = real_compute; aextra.rev= real_compute; aextra.stamp = now_time_t; aextra.tstamp = now_str;
+        struct alessio<struct extra> ablock;
+        ablock.data.push_back(aextra);
+        chain.push_back(ablock);
+    };
+
+    Alogic(const Alogic& alogic);
+    Alogic operator=(const Alogic& alogic);
+    std::vector<struct alessio<T> > get_blocks();
+    std::vector<struct alessio<T> > add_block(struct alessio<T>& newentry);
+    ssize_t max_block_size();
+    void display_chain_blocks();
+    private:  
+    std::vector<struct alessio<T> > chain;
+};
